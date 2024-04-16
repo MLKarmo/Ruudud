bbae56a41384f13e0a5204e47239a540cffe62ed
MLKarmo <148188843+MLKarmo@users.noreply.github.com>
2024-04-16 11:47:16 +0300

    UL3 Ruudud

--- ruudud.py
+++ ruudud.py
@@ -1,27 +1,33 @@
 import pygame
 import sys
 
+# ekraani suurus
 laius = 640
 pikkus = 480
 
-ruut = int(input("Ruudu suurus?: "))
-vahe = int(input("Vahede suurus?: "))
+# Ruutude ja vahede suurus
+ruut = 20
+vahe = 10
 
+#Värvid
 roosa = (250, 180, 180)
 kollane = [255, 255, 150]
 
+#Käivitab mängu
 pygame.init()
 
-screen = pygame.display.set_mode([640, 480])
+# Ekraani suurus ja programmi nimi
+screen = pygame.display.set_mode([laius, pikkus])
 pygame.display.set_caption("Harjutamine")
 
+# Ekraani värv
 screen.fill(kollane)
 
-
+# Arvutab kui palju ruute mahub ekraanile
 ruudud_x = laius // (ruut + vahe)
 ruudud_y = pikkus // (ruut + vahe)
 
-# Calculate actual vahe (remaining space after squares)
+# Näitab kui palju üle jääb
 laiuse_vahe = (laius - (ruudud_x * ruut)) // (ruudud_x - 1)
 pikkuse_vahe = (pikkus - (ruudud_y * ruut)) // (ruudud_y - 1)
 
@@ -32,8 +38,12 @@ for i in range(ruudud_x):
         pygame.draw.rect(screen, roosa, (x, y, ruut, ruut))
 
 pygame.display.flip()
-    
+
+#Suleb ristist vajatuses koheselt
 while True:
     if pygame.event.wait().type == pygame.QUIT:
         break
 pygame.quit()
+
+# Github:
+# https://github.com/MLKarmo/Ulesanded/blob/main/ruudud.py
